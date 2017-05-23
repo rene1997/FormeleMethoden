233a8e89863ba6dc4435a2200d4cb8285f3edf47
rene keijzer <renekeijzer1997@gmail.com>
2017-05-23 23:28:05 +0200

    table from ndfa

--- Automaat/NDFAToDFA.cs
+++ Automaat/NDFAToDFA.cs
@@ -42,24 +42,82 @@ namespace Automaat
         
 
 
-        private static SortedSet<Tuple<T, char, List<T>>> GetTable<T>(Automaat<T> ndfa) where T : IComparable
+        private static SortedSet<Tuple<T, char, T>> GetTable<T>(Automaat<T> ndfa) where T : IComparable
         {
-            var toStates = new SortedSet<Tuple<T,char,List<T>>>();
-            //get startstates:
-            foreach(var s in ndfa._states)
+            //get transitions (with epsilons)
+            var toStates = new SortedSet<Tuple<T,char,T>>();
+            foreach (var s in ndfa._states)
             {
-                var startStates = GetToStates(s, ndfa);
+                GetToStates(s, ndfa).ToList().ForEach(tuple => toStates.Add(new Tuple<T, char, T>(s, tuple.Item2, tuple.Item1)));
             }
-            
-            //get tostates:
 
+            //put it al together
+            var table = MergeDataToTable(toStates);
+            PrintTable(table);
+            
+            //get startstates:
+
+
+            //get tostates:
+            
             //get finalstates:
             return toStates;
         }
 
-        private static SortedSet<T> GetToStates<T>(T state, Automaat<T> automaat) where T: IComparable
+        private static SortedSet<Tuple<T,char>> GetToStates<T>(T state, Automaat<T> automaat) where T: IComparable
         {
-            return null;
+            var list = new SortedSet<Tuple<T,char>>();
+            foreach(var t in automaat._transitions)
+            {
+                if (t.FromState.Equals(state))
+                {
+                    if (t.IsEpsilonTransition())
+                    {
+                        GetToStates(t.ToState, automaat).ToList().ForEach(s => list.Add(s));
+                    }
+                    else
+                    {
+                        list.Add(new Tuple<T,char>(t.ToState, t.Symbol));
+                    }
+                }
+            }
+            return list;
+        }
+
+        private static SortedSet<Tuple<T, char, SortedSet<T>>> MergeDataToTable<T>(SortedSet<Tuple<T, char, T>> tostates) where T : IComparable
+        {
+            var table = new SortedSet<Tuple<T, char, SortedSet<T>>>();
+
+            foreach(var tuple in tostates)
+            {
+                bool added = false;
+                foreach(var row in table)
+                {
+                    if (tuple.Item1.Equals(row.Item1) && tuple.Item2 == row.Item2)
+                    {
+                        row.Item3.Add(tuple.Item3);
+                        added = true;
+                    }
+                }
+                if (!added)
+                {
+                    var newrow = new Tuple<T, char, SortedSet<T>>(tuple.Item1, tuple.Item2, new SortedSet<T>());
+                    newrow.Item3.Add(tuple.Item3);
+                    table.Add(newrow);
+                }
+            }
+            return table;
+        }
+
+        public static void PrintTable<T>(SortedSet<Tuple<T, char, SortedSet<T>>> table)
+        {
+            Console.WriteLine("state: \t\tsymbol:\t\tDestinations:");
+            foreach(var row in table)
+            {
+                string destinations = String.Empty;
+                row.Item3.ToList().ForEach(symbol => destinations += $"{symbol}, ");
+                Console.WriteLine($"{row.Item1}\t\t{row.Item2}\t\t{destinations}");
+            }
         }
 
     }
