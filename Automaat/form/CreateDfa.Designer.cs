2ed0beba237aa72062ec1b6c6ff6f76898cdd2d1
rene <renekeijzer1997@gmail.com>
2017-06-13 11:25:50 +0200

    form closing fix

--- Automaat/form/CreateDfa.Designer.cs
+++ Automaat/form/CreateDfa.Designer.cs
@@ -1,7 +1,19 @@
-﻿namespace Automaat.form
+﻿using System.Windows.Forms;
+
+namespace Automaat.form
 {
     partial class CreateDfa
     {
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
         /// <summary>
         /// Required designer variable.
         /// </summary>
